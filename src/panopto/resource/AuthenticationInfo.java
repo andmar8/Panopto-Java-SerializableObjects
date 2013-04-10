96dd0ca8376a968aa96c97fd86d7adc1f8388870
Andrew Martin <andrew.martin@ncl.ac.uk>
2013-04-10 15:50:56 +0100

    * Initial push

---
+++ src/panopto/resource/AuthenticationInfo.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of Panopto-Java-SerializableObjects.
+ * 
+ * Panopto-Java-SerializableObjects is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Panopto-Java-SerializableObjects is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Panopto-Java-SerializableObjects.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright: Andrew Martin, Newcastle University
+ * 
+ */
+package panopto.resource;
+
+//javax
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="authenticationInfo")
+public class AuthenticationInfo
+{
+    private String authCode;
+    private String password;
+    private String userKey;
+
+    public AuthenticationInfo(){}
+
+    public String getAuthCode() {
+        return authCode;
+    }
+
+    public void setAuthCode(String authCode) {
+        this.authCode = authCode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserKey() {
+        return userKey;
+    }
+
+    public void setUserKey(String userKey) {
+        this.userKey = userKey;
+    }
+}
